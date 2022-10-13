9703086b53efa37d7745a51c675315f5f0ccb304
Thein Htut <theinhtut.dec@gmail.com>
2022-10-13 21:03:42 +0800

    Added mock authentication

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,7 +1,10 @@
+using BuberDinner.Application.Services.Authentication;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 {
+    builder.Services.AddScoped<IAuthenticationService, AuthenticationService>();
     builder.Services.AddControllers();
 }
 var app = builder.Build();
---
+++ BuberDinner.Application/Services/Authentication/AuthenticationResult.cs
@@ -0,0 +1,8 @@
+﻿namespace BuberDinner.Application.Services.Authentication;
+
+public record AuthenticationResult(
+    Guid id,
+    string FirstName,
+    string LastName,
+    string Email,
+    string Token);
