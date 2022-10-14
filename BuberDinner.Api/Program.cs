cf00a8e3982240daf5b66c3cbc7e5e50e2317d57
Thein Htut <theinhtut.dec@gmail.com>
2022-10-14 16:30:11 +0800

    Added JwtTokenGenerator

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -7,7 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 {
     builder.Services
         .AddApplication()
-        .AddInfrastructure();
+        .AddInfrastructure(builder.Configuration);
     builder.Services.AddControllers();
 }
 var app = builder.Build();
--- BuberDinner.Application/Services/Authentication/AuthenticationService.cs
+++ BuberDinner.Application/Services/Authentication/AuthenticationService.cs
@@ -1,10 +1,36 @@
-﻿namespace BuberDinner.Application.Services.Authentication;
+﻿using BuberDinner.Application.Common.Interfaces.Authentication;
+
+namespace BuberDinner.Application.Services.Authentication;
 
 public class AuthenticationService : IAuthenticationService
 {
-    public AuthenticationResult Register(string firstName, string lastName, string email, string password)
+    private readonly IJwtTokenGenerator _jwtTokenGenerator;
+
+    public AuthenticationService(IJwtTokenGenerator jwtTokenGenerator)
     {
-        return new AuthenticationResult(Guid.NewGuid(), firstName, lastName, email, "token");
+        _jwtTokenGenerator = jwtTokenGenerator;
+    }
+
+    public AuthenticationResult Register(
+        string firstName,
+        string lastName,
+        string email,
+        string password)
+    {
+        // Check if user already exists
+
+        // Create user (GUID)
+
+        // Create JWT Token
+        Guid userId = Guid.NewGuid();
+
+        var token = _jwtTokenGenerator.GenerateToken(userId, firstName, lastName);
+
+        return new AuthenticationResult(userId,
+            firstName,
+            lastName,
+            email,
+            token);
     }
 
     public AuthenticationResult Login(string email, string password)
