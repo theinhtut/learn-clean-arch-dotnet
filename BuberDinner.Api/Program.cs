087e58469ca877242e1e3b3f4b1866b2ef6f469a
Thein Htut <theinhtut.dec@gmail.com>
2022-10-17 22:23:38 +0800

    Added error handling

--- BuberDinner.Api/Program.cs
+++ BuberDinner.Api/Program.cs
@@ -1,5 +1,8 @@
+using BuberDinner.Api.Errors;
+using BuberDinner.Api.Filters;
 using BuberDinner.Application;
 using BuberDinner.Infrastructure;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,10 +11,14 @@ var builder = WebApplication.CreateBuilder(args);
     builder.Services
         .AddApplication()
         .AddInfrastructure(builder.Configuration);
+    //builder.Services.AddControllers(options => options.Filters.Add<ErrorHandlingFilterAttribute>());
     builder.Services.AddControllers();
+    builder.Services.AddSingleton<ProblemDetailsFactory, BurberDinnerProblemDetailsFactory>();
 }
 var app = builder.Build();
 {
+    //app.UseMiddleware<ErrorHandlingMiddleware>(); // Using ErrorHandlingMiddleware
+    app.UseExceptionHandler("/error");
     app.UseHttpsRedirection();
     app.MapControllers();
     app.Run();
